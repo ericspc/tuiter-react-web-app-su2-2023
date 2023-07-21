52b8cae8920f7eeee4146fafc646598cc5962074
shipanchen <shipanchen71@gmail.com>
2023-07-20 22:22:33 -0400

    a3 commit

---
+++ src/labs/a3/working-with-arrays.js
@@ -0,0 +1,22 @@
+import ArrayIndexAndLength from "./array-index-and-length";
+import AddingAndRemovingDataToFromArrays from "./adding-and-removing-data-to-from-arrays";
+import ForLoops from "./for-loops";
+import MapFunction from "./map-function";
+import FindFunction from "./find-function";
+import FindIndexFunction from "./find-index";
+import FilterFunction from "./filter-function";
+function WorkingWithArrays() {
+    return (
+        <div>
+            <ArrayIndexAndLength/>
+            <AddingAndRemovingDataToFromArrays/>
+            <ForLoops/>
+            <MapFunction/>
+            <FindFunction/>
+            <FindIndexFunction/>
+            <FilterFunction/>
+        </div>
+    )
+}
+
+export default WorkingWithArrays
---
+++ src/tuiter/explore-screen.js
@@ -0,0 +1,9 @@
+import TuitSummaryList from "./tuit-summary-list";
+function ExploreScreen() {
+    return (
+        <div>
+            <TuitSummaryList/>
+        </div>
+    )
+}
+export default ExploreScreen;
